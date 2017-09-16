7fb11a9f44713ac553495bbf03c58ff6effd1755
marc <marc.vettiger@hotmail.com>
2017-09-16 12:04:36 +0200

    First time multi threading - test phase though

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main_download():
     for chunk in chunks[0:1]:
         res = DataEngine().get_update_data(chunk)
         #TODO: Better solution for this !!
-        break
+
         try:
             spread.append(spread_name, res)
         except gspread.exceptions.RequestError as e:
@@ -50,11 +50,66 @@ def print_top20_rated_spots(spots_df):
     spots_df.sort_values(dates, ascending=False).head(20)
 
 
+
+
+import threading
+
+exitFlag = 0
+
+
+class DownloadThread(threading.Thread):
+    def __init__(self, thread_id, chunk):
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.chunk = chunk
+
+    def run(self):
+        logger.info("Starting Thread %s" % self.thread_id)
+        res = DataEngine().get_update_data(self.chunk)
+        logger.info("Exiting Thread %s" % self.thread_id)
+
+
+def multithreaded_download():
+    """Multithread function to download new forecast data from MSW."""
+    spread_name = "RentepointDB"
+    logger.info("Starting multi threaded download")
+
+    ids = Spots().get_ids()
+
+    logger.info("Slicing list in even size chunks of 100ds")
+    chunks = [ids[i:i + 100] for i in xrange(0, len(ids), 100)]
+    logger.debug("Number of chunks: %s" % len(chunks))
+
+    thread_list = []
+    i = 1
+    for chunk in chunks:
+        thread = DownloadThread(i, chunk)
+        thread.start()
+        i += 1
+
+    # # Start all Threads
+    # thread1.start()
+    # thread2.start()
+
+         # TODO: Better solution for this !!
+
+    #     try:
+    #         spread.append(spread_name, res)
+    #     except gspread.exceptions.RequestError as e:
+    #         logger.error("Request Error in writing to Google Spread sheet:")
+    #         logger.error("Timeout 5s and trying again")
+    #         logger.error(e)
+    #         spread.append(spread_name, res)
+
+    logger.info("Finished application")
+
 if __name__ == '__main__':
     logging.config.fileConfig('cfg/logger.conf')
     logger = logging.getLogger()
 
     #pandas_load_example()
-    main_download()
+    #main_download()
+
+    multithreaded_download()
 
 
